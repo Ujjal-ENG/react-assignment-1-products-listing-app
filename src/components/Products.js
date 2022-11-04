4e46036675e27bacd6b2d99cdc0e96cf825e0205
user <ujjalroy7862@gmail.com>
2022-11-05 00:25:41 +0600

    Successfully done this assignment

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,14 @@
-/* eslint-disable react/prop-types */
+import React from "react";
+import Product from "./Product";
+
+const Products = ({ products }) => {
+  return (
+    <>
+      {products.map((product) => (
+        <Product product={product} key={Math.random()}/>
+      ))}
+    </>
+  );
+};
+
+export default Products;
